7fd17c1b8587fa51b68b3297f163c755abf20d7a
Alexandre <devxandeapp@gmail.com>
2025-04-24 12:28:04 -0300

    Fix: adjust variables name

--- supabase/functions/webhook_tiny/service/item_service.ts
+++ supabase/functions/webhook_tiny/service/item_service.ts
@@ -3,12 +3,11 @@ import { SupabaseServiceApi } from "./api/supabase_service_api.ts";
 import { SupabaseClient } from "https://esm.sh/@supabase/supabase-js";
 import { formatItemData } from "../utils/index .ts";
 
-
 class ItemService {
   private db: SupabaseServiceApi;
-  private supabase : SupabaseClient
+  private supabase: SupabaseClient;
   constructor(supabase: SupabaseClient) {
-    this.supabase = supabase
+    this.supabase = supabase;
     this.db = new SupabaseServiceApi(supabase);
   }
 
@@ -26,26 +25,27 @@ class ItemService {
   }
 
   async create(pedido_tiny: Pedido) {
-    const pedidoData: PedidoSupabase[] | null = await this.db.select(
+    const pedidoSupabase: PedidoSupabase[] | null = await this.db.select(
       "pedidos",
       "id_tiny",
       pedido_tiny.id,
     );
 
-    if (!pedidoData) {
+    if (!pedidoSupabase) {
       throw new Error(`Erro ao consultar em pedidos: ${pedido_tiny.id}`);
     }
-
-    const itemDataArray = pedido_tiny.itens.map((item) =>
-      formatItemData(item.item, pedido_tiny, this.supabase)
+    const itemDataArray = await Promise.all(
+      pedido_tiny.itens.map((item) =>
+        formatItemData(item.item, pedidoSupabase[0], this.supabase)
+      ),
     );
-
     try {
       await this.db.insert("itens", itemDataArray);
+      console.log("Novo pedido inserido:", pedido_tiny.numero);
     } catch (error) {
       console.error("Erro ao inserir em itens:", error);
+      throw new Error("Erro ao inserir itens. Pedido não foi finalizado.");
     }
-    console.log("Novo pedido inserido:", pedido_tiny.numero);
   }
 }
 
