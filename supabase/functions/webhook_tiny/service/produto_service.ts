ad3a211d94744899be3b6a337471e3918d67bf64
Alexandre <devxandeapp@gmail.com>
2025-04-24 12:31:37 -0300

    Fix: change logic for received supabase autheticated

--- supabase/functions/webhook_tiny/service/produto_service.ts
+++ supabase/functions/webhook_tiny/service/produto_service.ts
@@ -2,21 +2,23 @@ import { SupabaseClient } from "https://esm.sh/@supabase/supabase-js";
 import { ProdutoSupabase } from "../types/response_api_tiny/produto.ts";
 import { SupabaseServiceApi } from "./api/supabase_service_api.ts";
 
-
 class ProdutoService {
+  private db: SupabaseServiceApi;
+  private supabase: SupabaseClient;
 
-  private db: SupabaseServiceApi
-  private supabase : SupabaseClient
-  
   constructor(supabase: SupabaseClient) {
-    this.supabase = supabase
+    this.supabase = supabase;
     this.db = new SupabaseServiceApi(supabase);
   }
-  async select(id_produto_tiny: number): Promise<ProdutoSupabase[]| null> {
+  async select(id_produto_tiny: string): Promise<ProdutoSupabase[] | null> {
     try {
-      const produto: ProdutoSupabase[]| null = await this.db.select('pedidos', "id_tiny", id_produto_tiny);
-    
-     return produto
+      const produto: ProdutoSupabase[] | null = await this.db.select(
+        "produtos",
+        "id_tiny",
+        id_produto_tiny,
+      );
+
+      return produto;
     } catch (err) {
       console.error("Erro inesperado no select ProdutoControlle:", err);
       throw new Error(`Erro select ProdutoController: ${err.message}`);
